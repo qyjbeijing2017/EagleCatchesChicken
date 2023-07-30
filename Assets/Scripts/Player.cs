fb1a433e08cdc630e66493a0c670c4b2faf8af57
邱衍嘉 <qyjbeijing@gmail.com>
2023-07-30 09:04:55 +0800

    ADD: 增加了start场景和player类

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,13 +1,14 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using Unity.Netcode;
 
-public class Player : MonoBehaviour
+public class Player : NetworkBehaviour
 {
+    public NetworkVariable<Vector3> Position = new NetworkVariable<Vector3>();
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
