da4bc6d022c8bdf1d5c80bed92469dc72d1308a4
qyj <qyjbeijing@gmail.com>
2023-08-03 21:35:20 +0800

    add： 增加了一些按钮移动逻辑

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,19 +1,44 @@
 using UnityEngine;
 using Unity.Netcode;
+using UnityEngine.InputSystem;
 
 public class Player : NetworkBehaviour
 {
-    public NetworkVariable<Vector3> Position = new NetworkVariable<Vector3>();
+    private NetworkVariable<Vector3> Position = new NetworkVariable<Vector3>();
+    private NetworkVariable<Vector3> Forward = new NetworkVariable<Vector3>();
+
+    [SerializeField]
+    private float BaseMoveSpeed = 1f;
 
 
-    // Start is called before the first frame update
-    void Start()
+    static Vector3 GetRandomPositionOnPlane()
     {
+        return new Vector3(Random.Range(-3f, 3f), 1f, Random.Range(-3f, 3f));
+    }
+
+    public override void OnNetworkSpawn()
+    {
+        if (IsOwner)
+        {
+            var randomPosition = GetRandomPositionOnPlane();
+            transform.position = randomPosition;
+            Position.Value = randomPosition;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (IsOwner)
+        {
+            var inputAxis = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+            var moveSpeed = BaseMoveSpeed * Time.deltaTime;
+            var moveVector = inputAxis * moveSpeed;
+            var newPosition = transform.position + moveVector;
+            Position.Value = newPosition;
+            Forward.Value = moveVector.normalized;
+        }
+        transform.position = Position.Value;
+        transform.forward = Forward.Value;
     }
 }
