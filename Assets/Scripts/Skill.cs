be7736c255b01846052a0a5e1baaefd73dc8e96f
邱衍嘉 <qyjbeijing@gmail.com>
2023-08-10 01:24:09 +0800

    ADD: 增加基本技能逻辑

--- Assets/Scripts/Skill.cs
+++ Assets/Scripts/Skill.cs
@@ -1,19 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Mirror;
 
-[RequireComponent(typeof(Move))]
-[RequireComponent(typeof(Source))]
-[RequireComponent(typeof(BuffManager))]
-[RequireComponent(typeof(SkillManager))]
-public class Player : NetworkBehaviour
+public class NewBehaviourScript : MonoBehaviour
 {
-    [SyncVar]
-    public uint PlayerID;
     // Start is called before the first frame update
     void Start()
     {
+        
     }
 
     // Update is called once per frame
