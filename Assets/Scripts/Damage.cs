6a13d4240e148c5fcfe7cba9c60711f9dd89f8fa
邱衍嘉 <qyjbeijing@gmail.com>
2023-08-23 21:02:03 +0800

    MOD：修复NetworkIdentity的Bug去除测试Event

--- Assets/Scripts/Damage.cs
+++ Assets/Scripts/Damage.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using Mirror;
 
-[RequireComponent(typeof(NetworkIdentity))]
 [RequireComponent(typeof(Animator))]
 public abstract class Damage : NetworkBehaviour
 {
