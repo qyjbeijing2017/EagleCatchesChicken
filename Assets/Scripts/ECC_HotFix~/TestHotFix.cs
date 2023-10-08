3aa59a688a1f7e521100eb652dc10c7b049f1dcd
qyj <qyjbeijing@gmail.com>
2023-10-08 17:42:18 +0800

    ADD: 添加热更工程

---
+++ Assets/Scripts/ECC_HotFix~/TestHotFix.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace ECC_HotFix;
+public class TestHotFix
+{
+    public static void Test()
+    {
+        Debug.Log("TestHotFix.Test");
+    }
+}
